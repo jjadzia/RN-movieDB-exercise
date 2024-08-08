965066e0f23b7a8621cdca56be1014ff874709a8
jjadzia <jan.liberacki@leaware.com>
2024-08-08 19:34:13 +0200

    Add prettier to project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   {
-    ignores: ["node_modules/", "build/", "app-example/"],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    ignores: ['node_modules/', 'build/', 'app-example/'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
   },
+
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
